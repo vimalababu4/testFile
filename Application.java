439eec946e7905ed1ee760a88ca12c04c3ed521c
Vimala <vimalamoger@yahoo.com>
2021-06-07 14:28:34 -0500

    adding new java file to testfile

---
+++ Application.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+	public static void main(String [] args) {
+		int numAccounts=3;
+		
+		List<Account> accounts = Account.createAccounts(numAccounts);
+		for(int i=0;i<numAccounts;i++) {
+			System.out.println(accounts.get(i));
+		}
+
+		
+	}
+
+}
